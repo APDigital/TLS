9d504ccf8a1896073f01cfe4aa97083ba3af9362
APDigital <aoifejo.pearson@googlemail.com>
2018-01-30 11:46:35 +0000

    Three Letter Sequence Regex code

---
+++ TLS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TLS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = File.ReadAllText("Sample.txt");
+
+            var regexPat = new Regex("[tT][rR][aA]");
+
+            MatchCollection matches = regexPat.Matches(txt);
+
+            Console.WriteLine(matches.Count);
+            Console.ReadLine();
+        }
+    }
+}
